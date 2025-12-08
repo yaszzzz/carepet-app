8caa2c7ec280b3d4a7fa53fec6c0f335a3740ad1
Mohamad Ilyas Ali <108250794+yaszzzz@users.noreply.github.com>
2025-12-09 00:28:45 +0700

    fix

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./organisms/HeroSection/HeroSection";
 import { AboutSection } from "./organisms/AboutSection";
+import { ServicesSection } from "./organisms/ServicesSection";
 
 export default function Homesection() {
     return (
        <div>
          <HeroSection />
          <AboutSection />
+         <ServicesSection />
        </div>
     );
 }
--- components/organisms/ServicesSection/ServicesSection.tsx
+++ components/organisms/ServicesSection/ServicesSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/atoms/Button/Button';
